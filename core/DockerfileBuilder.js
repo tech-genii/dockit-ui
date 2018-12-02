bf7826793676e4135bc4158af51afa42c4c2e263
saif <saifullah.khan@99array.com>
2018-12-02 16:57:36 +0530

    Why: Commands need to be generated in order in which it was build

--- core/DockerfileBuilder.js
+++ core/DockerfileBuilder.js
@@ -2,6 +2,7 @@ import {CopyCommand, RunCommand, EntryPointCommand, EnvCommand} from "./Dockerfi
 
 class DockerfileBuilder{
     constructor() {
+        this.commands = [];
         this.runCommands = [];
         this.copyCommands = [];
         this.entryPointCommand = null;
@@ -20,7 +21,8 @@ class DockerfileBuilder{
         if (!(runCommand instanceof RunCommand)) {
             throw new Error("parameter should be an instance of RunCommand");
         }
-        this.runCommands.unshift(runCommand);
+        this.commands.push(runCommand);
+        // this.runCommands.unshift(runCommand);
         return this;
     }
 
@@ -28,7 +30,8 @@ class DockerfileBuilder{
         if (!(copyCommand instanceof CopyCommand)) {
             throw new Error("parameter should be an instance of CopyCommand");
         }
-        this.copyCommands.unshift(copyCommand);
+        this.commands.push(copyCommand);
+        // this.copyCommands.unshift(copyCommand);
         return this;
     }
 
@@ -51,15 +54,17 @@ class DockerfileBuilder{
     buildDockerfileString() {
         let dockerfileString = "";
         dockerfileString += "FROM "+this.baseImage+"\n";
-        this.copyCommands.unshift("");
-        dockerfileString += this.copyCommands.reduce((copyCommandString,currentCopyCommand)=>{return copyCommandString+"\n"+currentCopyCommand.getCommand()});
-        this.runCommands.unshift("");
+        dockerfileString += this.envs.getCommand();
         dockerfileString += "\n";
-        dockerfileString += this.runCommands.reduce((previousValue, currentValue) => previousValue+"\n"+currentValue.getCommand());
+        this.commands.unshift("");
+        this.copyCommands.unshift("");
+        dockerfileString += this.commands.reduce((prevCommandString,nextCommand)=>{return prevCommandString+"\n"+nextCommand.getCommand()});
+        // dockerfileString += this.copyCommands.reduce((copyCommandString,currentCopyCommand)=>{return copyCommandString+"\n"+currentCopyCommand.getCommand()});
+        // this.runCommands.unshift("");
+        // dockerfileString += "\n";
+        // dockerfileString += this.runCommands.reduce((previousValue, currentValue) => previousValue+"\n"+currentValue.getCommand());
         dockerfileString += "\n";
         dockerfileString += this.entryPointCommand.getCommand();
-        dockerfileString += "\n";
-        dockerfileString += this.envs.getCommand();
 
 
         return dockerfileString;
