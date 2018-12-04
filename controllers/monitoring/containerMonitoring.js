a44596ee06d81f71d1cc02ad029e43b13f339038
Alok <alok@Viveks-MacBook-Air.local>
2018-12-04 12:41:57 +0530

    Intergated CLI with few sample commands, lokking forward to build over it the actual commands

--- controllers/monitoring/containerMonitoring.js
+++ controllers/monitoring/containerMonitoring.js
@@ -12,8 +12,13 @@ cli
 .description('This command help you monitor your running containers')
 .action((containerId)=>{
     let header = [
-        { value: 'name', width: 30, headerAlign: 'left' },
-        { value: 'price', width: 30, headerAlign: 'left' }
+        { value: 'CID', width: 30, headerAlign: 'left' },
+        { value: 'IMAGE', width: 30, headerAlign: 'left' },
+        {value:'STATUS',width:30,headerAlign:'left'},
+        {value:'HEALTH',width:30,headerAlign:'left'},
+        {value:'START-TIME',width:60,headerAlign:'left'},
+        {value:'END-TIME',width:60,headerAlign:'left'},
+        {value:'STATE',width:30,headerAlign:'left'},
     ];
     
     const options = {
@@ -21,8 +26,9 @@ cli
     };
     
     const data = [
-        [`apple ${chalk.red("mac")}`,92.50],
-        ["ibm",120.15]
+        ['12843-Alok','image-641','RUNNING','GREEN','2018-12-04 11:04:32','NULL','READY'],
+        ['09322-Saif','image-097','RUNNING',`${chalk.red('RED')}`,'2018-12-03 11:04:32','2018-12-03 22:00:04','READY'],
+        ['93722-Spring','image-843','HALT',`${chalk.green('GREEN')}`,'2018-12-02 11:04:32','NULL','READY']
     ];
     
     let t3 = Table(header,data, options);
