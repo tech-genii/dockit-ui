ebe8ce47baa48fb8a2faead6eac30442d7fac2d9
saif <saifullah.khan@99array.com>
2018-12-18 01:10:24 +0530

    CLI created to read Dockitfile for development

--- src/dockit-core/index.ts
+++ src/dockit-core/index.ts
@@ -1,8 +1,36 @@
 import * as YAML from 'yaml'
 import * as fs from 'fs'
+import * as cli from 'commander';
+import * as shell from 'shelljs'
+import chalk from "chalk";
 
-let dockitConf:any = fs.readFileSync(__dirname+'/Dockit.yaml',"utf-8");
+const CWD = process.cwd();
+const DEFAULT_DOCKIT_CONF = '/Dockit.yaml';
+const DEFAULT_PATH = CWD+ DEFAULT_DOCKIT_CONF;
 
-console.log(dockitConf);
-dockitConf = YAML.parse(dockitConf);
-console.log(dockitConf.dockit.devImage);
+
+cli
+    .command("develop [dockitFile]")
+    .alias("d")
+    .description("This command starts the development environment")
+    .action(dockitFile => {
+        let dockitFilePath = CWD;
+        if (dockitFile) {
+            if (dockitFile.toString().trim().startsWith("/")) {
+                dockitFilePath = dockitFile.toString().trim();
+            }else {
+                dockitFilePath += "/"+dockitFile.toString().trim();
+            }
+        }else {
+            dockitFilePath = DEFAULT_PATH;
+        }
+        let dockitConf:any = fs.readFileSync(dockitFilePath,"utf-8");
+        const catConfig = shell.cat(dockitFilePath).stdout;
+        dockitConf = YAML.parse(dockitConf);
+        const exec = shell.exec("docker run -d " +dockitConf.dockit.devImage);
+        console.log(exec.code?chalk.bold.redBright(exec.stderr):chalk.bold.blueBright(exec.stdout));
+        console.log(dockitConf);
+        console.log(catConfig);
+    });
+
+cli.parse(process.argv);
