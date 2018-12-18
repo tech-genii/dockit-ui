e97915e72863eac9ab02393966e623c898a1846c
saif <saifullah.khan@99array.com>
2018-12-18 12:25:53 +0530

    dockerode used to start the container

--- src/dockit-core/index.ts
+++ src/dockit-core/index.ts
@@ -3,6 +3,9 @@ import * as fs from 'fs'
 import * as cli from 'commander';
 import * as shell from 'shelljs'
 import chalk from "chalk";
+import * as Docker from 'dockerode';
+
+const docker = new Docker();
 
 const CWD = process.cwd();
 const DEFAULT_DOCKIT_CONF = '/Dockit.yaml';
@@ -14,23 +17,28 @@ cli
     .alias("d")
     .description("This command starts the development environment")
     .action(dockitFile => {
-        let dockitFilePath:string = CWD;
+        let dockitFilePath: string = CWD;
         if (dockitFile) {
             if (dockitFile.toString().trim().startsWith("/")) {
                 dockitFilePath = dockitFile.toString().trim();
-            }else {
-                dockitFilePath += "/"+dockitFile.toString().trim();
+            } else {
+                dockitFilePath += "/" + dockitFile.toString().trim();
             }
-        }else {
+        } else {
             dockitFilePath = DEFAULT_PATH;
         }
-        let dockitConf:any = fs.readFileSync(dockitFilePath,"utf-8");
-        const catConfig = shell.cat(dockitFilePath).stdout;
+        let dockitConf: any = fs.readFileSync(dockitFilePath, "utf-8");
+        // const catConfig = shell.cat(dockitFilePath).stdout;
         dockitConf = YAML.parse(dockitConf);
-        const exec = shell.exec("docker run -d " +dockitConf.dockit.devImage);
-        console.log(exec.code?chalk.bold.redBright(exec.stderr):chalk.bold.blueBright(exec.stdout));
-        console.log(dockitConf);
-        console.log(catConfig);
+        docker.createContainer({
+            Image: dockitConf.dockit.devImage,
+            Tty: true,
+            Entrypoint:"/bin/bash"
+        }, (error1, container) => {
+            container.attach({stream: true, stdout: true, stderr: true}, function (err, stream) {
+                stream.pipe(process.stdout);
+            });
+        });
     });
 
 cli.parse(process.argv);
